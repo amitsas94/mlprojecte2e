f7bdea16cf7d7be096de80f42291a1ea7c3ec233
amitsas94 <amit.jssmech@gmail.com>
2024-05-26 13:03:55 +0530

    basic project str and logging and exception handling

---
+++ src/exception_hd.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def error_message_info(error, error_info:sys):
+    _,_,exc_traceback=error_info.exc_info()
+    file_name=exc_traceback.tb_frame.f_code.co_filename
+    error_info_message = "Error occured in python script name [{0}] at line number [{1}] with error message [{2}]".format(
+    file_name,exc_traceback.tb_lineno,str(error)
+    )
+    return error_info_message
+
+
+class CustomExceptionhd(Exception):
+    def __init__(self, error_info_message, error_info:sys):
+        super().__init__(error_info_message)
+        self.error_info_message=error_message_info(error_info_message,error_info=error_info)
+
+    def __str__(self):
+        return self.error_info_message
